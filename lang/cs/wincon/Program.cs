cc4c9987f89f7334a61fb5e3d85dbdf98fc9eceb
Mark Benvenuto <mark.benvenuto@mongodb.com>
2019-03-21 19:07:44 -0400

    snap

---
+++ lang/cs/wincon/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.MongoCrypt;
+
+namespace wincon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Version: " + Library.Version);
+        }
+    }
+}
